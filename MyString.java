027d3ff0cafa3681c87d0b9d0242250d59614d28
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-12-01 07:50:24 +0200

    Completed MyString.java

--- MyString.java
+++ MyString.java
@@ -18,13 +18,45 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newString = "";
+		for (int i = 0; i < (str.length()) ;i++) {
+            char c = str.charAt(i); 
+            if ((c >= 65) && (c <= 90)) {
+            	c += 32;
+				newString += c;       
+            } else if ((c == 32) || ((c >= 97) && ( c <= 122)) || (c >= 48) && ( c <= 57)) {
+				newString += c;
+			}
+        }
+        return newString;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        int count = 0;
+        boolean result = false;
+        int n = 0;
+        if (str2.length() == 0) {
+            return true;
+        }
+        if (str1.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < str1.length() - str2.length() ; i++) {
+            if (str1.charAt(i) == str2.charAt(n)) {
+                    for (int j = 0; j < str2.length(); j++) {
+                        if (str1.charAt(i + j) == str2.charAt(j)) {
+                            count++;
+                        } else if (str1.charAt(i) != str2.charAt(i)) {
+                            count = 0;
+                        }
+                    }
+                }
+                if (count == str2.length()) {
+                    result = true;
+                    }
+            }
+        return result;
     }
 }
+
